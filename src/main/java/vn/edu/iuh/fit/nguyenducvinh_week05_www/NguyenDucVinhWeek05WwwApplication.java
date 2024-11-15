566375d260f8634afae08e7662d66c30b8f1e968
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2024-11-15 15:54:08 +0700

    first commit

---
+++ src/main/java/vn/edu/iuh/fit/nguyenducvinh_week05_www/NguyenDucVinhWeek05WwwApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.nguyenducvinh_week05_www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenDucVinhWeek05WwwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenDucVinhWeek05WwwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/nguyenducvinh_week05_www/NguyenDucVinhWeek05WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.nguyenducvinh_week05_www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenDucVinhWeek05WwwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
